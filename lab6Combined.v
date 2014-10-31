eabb8ef35b7e7e8fa616282b340dc9fb7fbf65c9
Tasdiq Ameem <Remian8985@users.noreply.github.com>
2014-10-31 00:24:11 -0400

    Update lab6Combined.v

--- lab6Combined.v
+++ lab6Combined.v
@@ -6,19 +6,52 @@ module part1(SW, KEY, LEDR, LEDG);
 	output [17:0] LEDR;
 	output [7:0] LEDG;
 
-	wire [6:0] pres_state;
-	wire [6:0] next_state;
+//	wire [6:0] pres_state;
+//	wire [6:0] next_state;
+
+	reg [6:0] pres;
 	
+	wire [6:0]m ;
+//	wire [6:0]uu ;
+//	assign uu = pres;
+	
+	assign LEDR[16:10] = m[6:0];
+//	wire a, b, c, d, e, f, g;
+		assign LEDR[6:0] = pres[6:0];
+//		assign LEDG[0] = m[6];
+//	assign LEDG[0] = pres_state[6];
 	
 //	assign LEDR [16:10] = next_state[6:0];
+	assign m[0] = (pres[0] | pres[1] | pres[4] | pres[6]) & ~SW[1];
+	assign m[1] = (pres[0] ) & SW[1];
+	assign m[2] = (pres[1]  | pres[6]) & SW[1];
+	assign m[3] = (pres[2] ) & SW[1];
+	assign m[4] = (pres[2] | pres[3] | pres[5]) & ~SW[1];
+	assign m[5] = (pres[3] | pres[5] ) & SW[1];
+	assign m[6] = (pres[4] ) & SW[1];
+
+assign LEDG[0] = pres[6];
+
+
+always@(posedge KEY[0]) begin 
+	if (!SW[0]) begin 
+		pres <= 7'b0000001;	
+	end 
+
+	else begin 
+		pres <= m;
+	end 
+
+end 
+
+
+
+
+//	subCircuit jj(pres_state, SW[1], next_state);
+//	parallelLoad_flipflop ff(next_state[6:0], KEY[0], SW[0], pres_state[6:0]);
+	
 	
 
-	subCircuit jj(pres_state, SW[1], next_state);
-	parallelLoad_flipflop ff(next_state[6:0], KEY[0], SW[0], pres_state[6:0]);
-	
-	
-	assign LEDR[6:0] = pres_state[6:0];
-	assign LEDG[0] = pres_state[6];
 endmodule 
 
 
@@ -31,7 +64,7 @@ module subCircuit(pres, W, m );
 	assign m[0] = (pres[0] | pres[1] | pres[4] | pres[6]) & ~W;
 	assign m[1] = (pres[0] ) & W;
 	assign m[2] = (pres[1]  | pres[6]) & W;
-	assign m[3] = (pres[4] ) & W;
+	assign m[3] = (pres[2] ) & W;
 	assign m[4] = (pres[2] | pres[3] | pres[5]) & ~W;
 	assign m[5] = (pres[3] | pres[5] ) & W;
 	assign m[6] = (pres[4] ) & W;
@@ -47,10 +80,33 @@ module parallelLoad_flipflop(D, clk, resetn, Q);
 	always@(posedge clk)
 	begin
 		if (!resetn)
-			Q[0] <= 1;
+			Q[0] <= 7'b0000001;
 		else
 			Q[6:0] <= D[6:0];
 	end 
 endmodule 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+module decode (num, coded);
+input [2:0] num;
+output reg [13:0] coded;
+
+
+endmodule 
